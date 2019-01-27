e3201270c4479b698b9340dda5036e68f8151898
Paweł Iwaneczko <p.iwaneczko@gmail.com>
2019-01-27 13:01:01 +0100

    Certificate update. Added scripts certs.ps1

---
+++ certs.ps1
@@ -0,0 +1,11 @@
+﻿$Subject = "E=p.iwaneczko@aircom.ag
+            OU=www.aircom.ag
+            CN=Aircom AI"
+
+$cacert = (Get-ChildItem Cert:\CurrentUser\Root\135CEC36F49CB8E93B1AB270CD80884676CE8F33)
+
+$Cert = New-SelfSignedCertificate -Subject $Subject -CertStoreLocation Cert:\CurrentUser\My\ -Type Codesigning
+
+Export-Certificate -Cert $Cert -FilePath ppremote.cer
+
+Set-AuthenticodeSignature .\ppremote.cer -Certificate $cacert
