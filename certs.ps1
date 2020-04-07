5ba761c651f74871033932acbc829f43b5649e1c
Paweł Iwaneczko <piwaneczko@cadm-a.com>
2020-04-07 14:23:59 +0200

    Changed certificate with private Subject

--- certs.ps1
+++ certs.ps1
@@ -1,8 +1,11 @@
-﻿$Subject = "E=p.iwaneczko@gmai.com CN=Pawel Iwaneczko S=Poland C=PL"
-
+﻿$Subject = "CN=Pawel Iwaneczko, 
+            E=p.iwaneczko@gmail.com,
+            OU=https://github.com/piwaneczko/PPLaserRemoteServer/releases,
+            S=Poland,
+            C=PL"
 #$cacert = (Get-ChildItem Cert:\CurrentUser\Root\thumbprint)
 
-#$cert = New-SelfSignedCertificate -Subject $Subject -CertStoreLocation Cert:\CurrentUser\My\ -Type Codesigning
+#New-SelfSignedCertificate -Subject $Subject -CertStoreLocation Cert:\CurrentUser\My\ -Type Codesigning
 $cert = (Get-ChildItem Cert:\CurrentUser\My -CodeSigningCert)
 
 Export-Certificate -Cert $cert -FilePath ppremote.cer
